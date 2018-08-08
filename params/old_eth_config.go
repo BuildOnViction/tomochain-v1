f5238dc40593cd9a50af654792024cdeb1e4e2a9
Nguyen Ba Tam <nguyenbatam90@gmail.com>
2018-08-08 16:28:15 +0700

    add again clique config to params package

---
+++ params/old_eth_config.go
@@ -0,0 +1,6 @@
+package params
+// CliqueConfig is the consensus engine configs for proof-of-authority based sealing.
+type CliqueConfig struct {
+	Period uint64 `json:"period"` // Number of seconds between blocks to enforce
+	Epoch  uint64 `json:"epoch"`  // Epoch length to reset votes and checkpoint
+}
